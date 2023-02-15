6b47146ed8325e017b9320e94c0ff93aa6423002
APrzybyla <106275293+Albert-Przybyla@users.noreply.github.com>
2023-02-15 18:27:56 +0100

    add photo sorting

---
+++ src/app/models/photo.model.ts
@@ -0,0 +1,14 @@
+export class Photo {
+}
+
+export class PhotoChunks {
+    chunk1: any[]
+    chunk2: any[]
+    chunk3: any[]
+
+    constructor() {
+        this.chunk1 = []
+        this.chunk2 = []
+        this.chunk3 = []
+    }
+}
--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { PhotoService } from '../services/photo.service';
+import { PhotoChunks } from '../models/photo.model';
 
 @Component({
   selector: 'app-results',
@@ -14,6 +15,10 @@ export class ResultsComponent implements OnInit, OnChanges {
 
   public photos: any[] = [];
 
+  public photoChunks: PhotoChunks = new PhotoChunks()
+
+  public rowHeight = [0, 0, 0];
+
   constructor(private _photo: PhotoService) { }
 
   ngOnInit(): void {
@@ -26,11 +31,57 @@ export class ResultsComponent implements OnInit, OnChanges {
     }
   }
 
+  private filter(photo: any) {
+    let photoHeight: number = photo.height / photo.width;
+    let pushTo: number = 0;
+    for (let i = 0; i < 3; i++) {
+      if (this.rowHeight[i] <= this.rowHeight[(i + 1) % 3] && this.rowHeight[i] <= this.rowHeight[(i + 2) % 3]) {
+        pushTo = i;
+      }
+    }
+
+    switch (pushTo) {
+      case 0:
+        this.photoChunks.chunk1.push(photo);
+        this.rowHeight[0] += photoHeight;
+        break;
+      case 1:
+        this.photoChunks.chunk2.push(photo);
+        this.rowHeight[1] += photoHeight;
+        break;
+      case 2:
+        this.photoChunks.chunk3.push(photo);
+        this.rowHeight[2] += photoHeight;
+        break;
+    }
+    //   if (this.rowHeight[0] <= this.rowHeight[1] && this.rowHeight[0] <= this.rowHeight[2]) {
+    //     this.photoChunks.chunk1.push(photo);
+    //     this.rowHeight[0] += photoHeight;
+    //     console.log(this.rowHeight)
+    //     return;
+    //   }
+
+    //   if (this.rowHeight[1] <= this.rowHeight[0] && this.rowHeight[1] <= this.rowHeight[2]) {
+    //     this.photoChunks.chunk2.push(photo);
+    //     this.rowHeight[1] += photoHeight;
+    //     console.log(this.rowHeight)
+    //     return;
+    //   }
+
+    //   if (this.rowHeight[2] <= this.rowHeight[1] && this.rowHeight[2] <= this.rowHeight[0]) {
+    //     this.photoChunks.chunk3.push(photo);
+    //     this.rowHeight[2] += photoHeight;
+    //     console.log(this.rowHeight)
+    //     return;
+    //   }
+  }
+
   private loadPhoto() {
     this.loading = true;
     this._photo.photos().subscribe(obj => {
-      this.photos = obj;
-      console.log(this.photos)
+      for (let i = 0; i < obj.length; i++) {
+        this.filter(obj[i])
+      }
       this.loading = false;
     })
   }
